cd6bb70b2817daa4f535e9fba7632deee0fd072a
Rogem Lepasana <rogemlepasana@gmail.com>
2025-04-26 09:34:34 +0800

    Fixed request

--- resources/views/content/apps/partials/succession-planning-request-table.blade.php
+++ resources/views/content/apps/partials/succession-planning-request-table.blade.php
@@ -9,7 +9,7 @@
         };
     @endphp
     <tr>
-        <td class="text-center">{{ $request->user->name }}</td>
+        <td class="text-center">{{ $request?->user?->name }}</td>
         <td class="text-start">
             <span class="badge rounded {{ $status }}">
                 {{ $request->status }}
